57b03d1cb50f0a93dadb14ee4678756ce3701b6a
ThorFA <thorfa4444@gmail.com>
2022-04-07 19:43:57 +0200

    Fixed sendFile

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 app.use(express.static("public"));
 app.get("/", (req, res) => {
-    res.send("/KEA-Plan.html");
+    res.sendFile(__dirname + "/KEA-Plan.html");
 });
 app.listen(process.env.PORT || 3456);
