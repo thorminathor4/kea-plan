adc29616d8244f8a1518b6cf6f1beb6c80d3331f
ThorFA <thorfa4444@gmail.com>
2022-04-07 12:51:10 +0200

    Restructured

--- public/LessonScript.js
+++ public/LessonScript.js
@@ -1,107 +1,154 @@
 let currentDepartment, currentStudentSet;
+let firstHour = 8, firstMinute = 30;
 
 loadEmptyDays()
 
 function loadEmptyDays(){
 	let timeslot0 = document.getElementById('timeslot0');
-	for(let i = 0; i < 5; i++)
-		timeslot0.innerHTML += `<td class="break day${i}" rowspan="29"></td>`;
+	for(let i = 0; i < 5; i++){
+		let emptyDay = createBreakElement(i, 29);
+		timeslot0.appendChild(emptyDay);
+		//timeslot0.innerHTML += `<td class="break day${i}" rowspan="29"></td>`;
+	}
 }
 
 //Uses: departments, currentDepartment
 //Calls: clearDays(), getTimeslot(hour, minute), breakToHtml(day, timeslotLength), lessonToHtml(lesson, timeslotLength)
 function loadStudentSet(){
-
 	let studentSetTitle = document.getElementById('student-set').value;
 	currentStudentSet = studentSetTitle;
 	document.querySelector('.student-set-title').innerText = studentSetTitle;
-	document.querySelector('title').innerText = 'KEA Plan - ' + studentSetTitle;
+	document.querySelector('title').innerText = `KEA Plan - ${studentSetTitle}`;
 	clearDays();
-
 	let filePath = `./Departments/${currentDepartment}/${studentSetTitle}.json`;
-	console.log(filePath);
 
 	fetch(filePath)
-	.then(response => response.json())
+	.then(response => { if(response.ok) return response.json(); })
 	.then(studentSet => {
+		if(studentSet){
+			document.getElementById('student-set-error').innerText = '';
+			let lastBreaks = [0, 0, 0, 0, 0];
+			let scheduleMatrix = [];
 
-		console.log(studentSet);
+			for(let i = 0; i < 29; i++)
+				scheduleMatrix.push([]);
 
-		let lastBreaks = [0, 0, 0, 0, 0];
-		let scheduleMatrix = [];
-
-		for(let i = 0; i < 29; i++)
-			scheduleMatrix.push([]);
-
-		for(let i in studentSet.lessons)
-			addLessonToSchedule(scheduleMatrix, studentSet.lessons[i], lastBreaks);
-
-		addLessonsToTable(scheduleMatrix);
-
-	}).catch(error => {
-		console.error(error);
-		document.getElementById('student-set-error').innerText = 'Student Set Not Present';
-		loadEmptyDays();
+			for(let i in studentSet.lessons)
+				addLessonToSchedule(scheduleMatrix, studentSet.lessons[i], lastBreaks);
+			
+			addLessonsToTable(scheduleMatrix);
+		}else{
+			document.getElementById('student-set-error').innerText = 'Student Set Not Present';
+			loadEmptyDays();
+		}
 	});
 }
 
-function addLessonToSchedule(scheduleMatrix, lesson){
-	let timespan = lesson.timespan;
-	let startTimeslot = getTimeslot(timespan[0], timespan[1]);
-	let endTimeslot = getTimeslot(timespan[2], timespan[3]);
-	let timeslotLength = endTimeslot - startTimeslot;
+function addLessonToSchedule(scheduleMatrix, lesson, lastBreaks){
+	let startTimeslot = getTimeslotIndex(lesson.starts);
+	let endTimeslot = getTimeslotIndex(lesson.ends);
 
-	if(startTimeslot > 0)
+	//Remove last break added to the day if there is one and the lesson doesn't start in the first timeslot
+	if(startTimeslot > 0 && scheduleMatrix.length > 0)
 		scheduleMatrix[lastBreaks[lesson.day]].pop();
 	
+	//Add a new break before the lesson if there is a break before the lesson
 	if(startTimeslot > lastBreaks[lesson.day]){
-		let breakHtml = breakToHtml(lesson.day, startTimeslot - lastBreaks[lesson.day]);
-		scheduleMatrix[lastBreak[lesson.day]].push(breakHtml);
+		let breakElement = createBreakElement(lesson.day, startTimeslot - lastBreaks[lesson.day]);
+		scheduleMatrix[lastBreaks[lesson.day]].push(breakElement);
 	}
 	
-	let lessonHtml = lessonToHtml(lesson, timeslotLength);
-	scheduleMatrix[startTimeslot].push(lessonHtml);
+	//Add the lesson
+	let lessonElement = createLessonElement(lesson, endTimeslot - startTimeslot);
+	scheduleMatrix[startTimeslot].push(lessonElement);
 
+	//Add a new break after the lesson if the lesson doesn't end in the last timeslot
 	if(endTimeslot < 28){
-		let breakHtml = breakToHtml(lesson.day, 29 - endTimeslot);
-		scheduleMatrix[endTimeslot].push(breakHtml);
+		let breakElement = createBreakElement(lesson.day, 29 - endTimeslot);
+		scheduleMatrix[endTimeslot].push(breakElement);
 		lastBreaks[lesson.day] = endTimeslot;
 	}
 }
 
 function addLessonsToTable(scheduleMatrix){
-	for(let row in scheduleMatrix){
-		let timeslotElements = scheduleMatrix[row];
-		let timeslot = document.getElementById('timeslot' + row);
-		for(let col in timeslotElements){
-			timeslot.innerHTML += timeslotElements[col];
-			if(timeslot.lastChild.rowSpan < 3)
-				timeslot.lastChild.style.padding = "0 10px";
-		}
+	for(let timeslot in scheduleMatrix){
+		let timeslotElements = scheduleMatrix[timeslot];
+		let timeslotElement = document.getElementById(`timeslot${timeslot}`);
+		for(let col in timeslotElements)
+			timeslotElement.appendChild(timeslotElements[col]);
 	}
 }
 
 function clearDays(){
 	for(let i = 0; i < 29; i++){
-		let timeslot = document.getElementById('timeslot' + i);
+		let timeslot = document.getElementById(`timeslot${i}`);
 		while(timeslot.childNodes.length > 1)
 			timeslot.removeChild(timeslot.lastChild);
 	}
 }
 
-function getTimeslot(hour, minute){
-	return (hour - 8) * 4 - 2 + (minute / 15);
+function getTimeslotIndex(timeString){
+	let {hour, minute} = parseTimeString(timeString);
+	return (hour - firstHour) * 4 + (minute - firstMinute) / 15;
 }
 
-function breakToHtml(day, timeslotLength){
-	return `<td class="break day${day}" rowspan="${timeslotLength}"></td>`;
+function parseTimeString(timeString){
+	let colonIndex = timeString.indexOf(':');
+	let hour = Number(timeString.slice(0, colonIndex));
+	let minute = Number(timeString.slice(colonIndex + 1, timeString.length));
+	return {hour, minute};
 }
 
-function lessonToHtml(lesson, timeslotLength){
-	let timespan = lesson.timespan;
-	for(let i in timespan)
-		timespan[i] = String(timespan[i]).padStart(2, '0');
-	let timeText = `${timespan[0]}:${timespan[1]} - ${timespan[2]}:${timespan[3]}`;
+function createBreakElement(day, timeslotLength){
+	let breakElement = document.createElement('td');
+	breakElement.classList.add('break', `day${day}`);
+	breakElement.rowSpan = timeslotLength;
+	return breakElement;
+}
+
+function createLessonElement(lesson, timeslotLength){
+	//Create lesson element
+	let lessonElement = document.createElement('td');
+	lessonElement.classList.add('lesson', `day${lesson.day}`);
+	lessonElement.rowSpan = timeslotLength;
+
+	//Change padding for short lessons
+	if(timeslotLength < 3)
+		lessonElement.style.padding = "0 10px";
+
+	//Add the start time and end time
+	lessonElement.innerText = `${lesson.starts} - ${lesson.ends}`;
+	
+	//Add seperation
+	lessonElement.appendChild(timeslotLength < 5 ? document.createTextNode(', ') : document.createElement('br'));
+
+	//Add the lesson title
+	let lessonTitle = document.createElement(timeslotLength < 5 ? 'b' : 'h3');
+	lessonTitle.innerText += lesson.title;
+	lessonElement.appendChild(lessonTitle);
+
+	if(timeslotLength > 1){
+		//Add seperation
+		if(timeslotLength < 5)
+			lessonElement.appendChild(document.createElement('br'));
+
+		//Add lesson teacher
+		lessonElement.appendChild(document.createTextNode(lesson.teachers[0]));
+		if(lesson.teachers.length > 1)
+			for(let i = 1; i < lesson.teachers.length; i++){
+				lessonElement.appendChild(document.createElement('br'));
+				lessonElement.appendChild(document.createTextNode(lesson.teachers[i]));
+			}
+
+		//Add seperation
+		lessonElement.appendChild(timeslotLength < 5 ? document.createTextNode(', ') : document.createElement('br'));
+		
+		//Add lesson room
+		lessonElement.appendChild(document.createTextNode(lesson.room));
+	}
+	return lessonElement;
+
+	/*
 	let innerHtml;
 	if(timeslotLength < 2)
 		innerHtml = `${timeText}, <b>${lesson.title}</b>`;
@@ -110,6 +157,7 @@ function lessonToHtml(lesson, timeslotLength){
 	else
 		innerHtml = `${timeText}<br><h3>${lesson.title}</h3>${lesson.teacher}<br>${lesson.room}`;
 	return `<td class="lesson day${lesson.day}" rowspan="${timeslotLength}">${innerHtml}</td>`;
+	*/
 }
 
 //Calls: currentDepartment, studentSetToHtmlOption(studentSet, index)
